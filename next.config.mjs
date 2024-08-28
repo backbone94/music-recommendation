85206833999650166117d8c6fa6305953b0dd647
leejeongjun <backbone94@naver.com>
2024-08-28 17:48:31 +0900

    chore: 이미지 도메인 추가 및 strict mode 비활성화

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['avatars.githubusercontent.com', 'img1.kakaocdn.net'],
+    domains: ['avatars.githubusercontent.com', 'img1.kakaocdn.net', 'i.scdn.co'],
   },
+  reactStrictMode: false,
 };
 
 export default nextConfig;
